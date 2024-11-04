874540d12e04852cc7ea854f610f6c8749290457
Leonardo <leonardorscarpitta@gmail.com>
2024-11-04 10:41:16 -0300

    feat: reorganize main components For a better code maintance 'App.jsx' will only contain Header, Home & Footer components. The 'Home.jsx' component will handle main components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import Banner from "./components/Banner";
-import Cards from "./components/Cards";
-import Carrossel from "./components/Carrossel";
-import Footer from "./components/Footer";
-import Formulario from "./components/Formulario";
-import Header from "./components/Header";
+import { Footer, Home, Header } from "./components/index.js";
 
 export default function App() {
   return (
-    <main className="flex flex-col gap-y-8 mx-56">
+    <>
       <Header />
-      <hr />
-      <Banner />
-      <hr />
-      <Carrossel />
-      <hr />
-      <Cards />
-      <hr />
-      <Formulario />  
+      <Home />
       <Footer />
-    </main>
+    </>
   )
 }
